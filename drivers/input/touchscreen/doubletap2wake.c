c9160e03c192f1859aa57a7cc4d2295002993777
thewisenerd <thewisenerd@protonmail.com>
2016-05-29 00:20:41 +0530

    s2w, dt2w: add melfas_ts input device in input_dev_filter

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -222,7 +222,7 @@ static void dt2w_input_event(struct input_handle *handle, unsigned int type,
 }
 
 static int input_dev_filter(struct input_dev *dev) {
-	if (strstr(dev->name, "touch")) {
+	if (strstr(dev->name, "touch") || strstr(dev->name, "touch_mcs8000")) {
 		return 0;
 	} else {
 		return 1;
